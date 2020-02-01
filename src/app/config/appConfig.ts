4fbb6484e006d568f97ca20c903bfd164fef0df8
Tyler McCracken <tylerdean.w@gmail.com>
2020-01-31 20:20:27 -0500

    pointer-events test

--- src/app/config/appConfig.ts
+++ src/app/config/appConfig.ts
@@ -45,6 +45,49 @@ export const AppConfig = {
             }
           ]
         }
+      },
+      {
+        label: 'Parks',
+        id: 'parks',
+        url: 'https://webgis.durhamnc.gov/server/rest/services/PublicServices/Community/MapServer/8',
+        popupTemplate: {
+          // autocasts as new PopupTemplate()
+          title: '{NAME}',
+          content: [
+            {
+              // It is also possible to set the fieldInfos outside of the content
+              // directly in the popupTemplate. If no fieldInfos is specifically set
+              // in the content, it defaults to whatever may be set within the popupTemplate.
+              type: 'fields',
+              fieldInfos: [
+                {
+                  fieldName: 'PARKURL',
+                  label: '<b>Name</b>'
+                },
+                {
+                  fieldName: 'TRAILTYPE',
+                  label: '<b>Trail type</b>'
+                },
+                {
+                  fieldName: 'SURFACETYPE',
+                  label: '<b>Surface type</b>'
+                },
+                {
+                  fieldName: 'STATUS',
+                  label: '<b>Status</b>'
+                },
+                {
+                  fieldName: 'LENGTH',
+                  label: '<b>Length (mi)</b>'
+                },
+                {
+                  fieldName: 'WIDTH',
+                  label: '<b>Width (ft)</b>'
+                }
+              ]
+            }
+          ]
+        }
       }
     ],
   }
