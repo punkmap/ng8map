79bcdb10e226295114fa4423d120c641e59f9228
Tyler McCracken <tylerdean.w@gmail.com>
2020-03-17 08:43:16 -0400

    toggle menu button styling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { NavbarService } from '../services/sidenav.service';
+import { Component, OnInit } from "@angular/core";
+import { NavbarService } from "../services/sidenav.service";
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
+  toggleState = false;
 
   constructor(private navBarService: NavbarService) { }
 
   ngOnInit() {
   }
   navbarSideMenuSelect() {
-    this.navBarService.toggle()
+    this.toggleState = !this.toggleState;
+    this.navBarService.toggle();
   }
-
 }
